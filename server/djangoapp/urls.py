c65bb9dec49a92e941f8903bf4985a1fe2e34e22
Liana003003 <evangelia.alevrakis@gmail.com>
2026-03-03 10:31:52 -0500

    register

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,12 +8,12 @@ from django.views.generic import TemplateView
 app_name = 'djangoapp'
 urlpatterns = [
     # # path for registration
-
+    path(route ='register', view=views.registration, name='register'),
     # path for login
    
     path(route='login', view=views.login_user, name='login'),
     
-
+    path(route='logout', view=views.logout_request, name='logout'),
     # path for dealer reviews view
 
     # path for add a review view
