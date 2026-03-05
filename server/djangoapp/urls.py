e2db338e254c5ae5f6ecbe83b845f745d6a9b515
Liana003003 <evangelia.alevrakis@gmail.com>
2026-03-05 05:04:31 -0500

    registering car model

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -7,13 +7,12 @@ from django.views.generic import TemplateView
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # # path for registration
-    path(route ='register', view=views.registration, name='register'),
-    # path for login
-   
-    path(route='login', view=views.login_user, name='login'),
     
+    path(route ='register', view=views.registration, name='register'),
+    path(route='login', view=views.login_user, name='login'),
     path(route='logout', view=views.logout_request, name='logout'),
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
+
     # path for dealer reviews view
 
     # path for add a review view
